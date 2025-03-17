eeff9b20358786526b8f9d309dacd17745e6373c
zwy <zhongweiyu0812@163.com>
2025-03-17 22:59:34 +0800

    feat: 页面初始化

--- src/index.js
+++ src/index.js
@@ -6,10 +6,21 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <div>hello world</div>
+    <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+const HomePage = () => {
+    return (
+        <div>
+            hello world
+        </div>
+    );
+};
+
+export default HomePage;
---
+++ src/pages/comments/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CommentsPage = () => {
+    return (
+        <div>
+            <h1>评论</h1>
+        </div>
+    );
+};
+
+export default CommentsPage;
