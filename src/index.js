0b1c029cf14fbd8d08fad1db88086324ce2ec9d5
右边 <2448685828@qq.com>
2025-03-17 14:39:04 +0000

    add Register.js

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- <div>hello world</div>
+ <App />
 );
 
 // If you want to start measuring performance in your app, pass a function
