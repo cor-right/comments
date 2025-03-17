47752ff0c1f37a1f953d48e7277c109d1a7178b4
右边 <2448685828@qq.com>
2025-03-17 15:52:02 +0000

    rewrite login

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,85 +1,67 @@
-import React from 'react';
 import { LockOutlined, UserOutlined } from '@ant-design/icons';
-import { Link, useNavigate } from 'react-router-dom';
-import { Button, Checkbox, Form, Input, Flex, message } from 'antd';
+import { Button, Checkbox, Form, Input } from 'antd';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const LoginPage = () => {
-    const navigate = useNavigate();
-
     const onFinish = (values) => {
-        console.log('Form values:', values); // 添加调试信息
-
-        // 模拟登录验证
-        if (values.username === 'admin' && values.password === 'password') {
-            // 登录成功，保存登录态到 localStorage
-            localStorage.setItem('isLoggedIn', 'true');
-            localStorage.setItem('username', values.username);
-
-            // 重定向到首页或其他页面
-            navigate('/comments');
-        } else {
-            message.error('Login failed'); // 使用 antd 的 message 组件显示错误信息
-        }
-    };
-
-    const onFinishFailed = (errorInfo) => {
-        console.log('Failed:', errorInfo); // 添加失败时的调试信息
-    };
-
-    return (
+        console.log('Received values of form: ', values);
+      };
+      return (
         <Form
-            name="login"
-            initialValues={{
-                remember: true,
-            }}
-            style={{
-                maxWidth: 360,
-                margin: 'auto',
-            }}
-            onFinish={onFinish}
-            onFinishFailed={onFinishFailed} // 添加 onFinishFailed 处理程序
+          name="normal_login"
+          className="login-form"
+          initialValues={{
+            remember: true,
+          }}
+          style={{ 
+            maxWidth: 360,
+            margin: 'auto'
+          }}
+          onFinish={onFinish}
         >
-            <Form.Item
-                name="username"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Username or E-mail!',
-                    },
-                ]}
-            >
-                <Input prefix={<UserOutlined />} placeholder="Username or E-mail" />
+          <Form.Item
+            name="username"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Username!',
+              },
+            ]}
+          >
+            <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[
+              {
+                required: true,
+                message: 'Please input your Password!',
+              },
+            ]}
+          >
+            <Input
+              prefix={<LockOutlined className="site-form-item-icon" />}
+              type="password"
+              placeholder="Password"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Form.Item name="remember" valuePropName="checked" noStyle>
+              <Checkbox>Remember me</Checkbox>
             </Form.Item>
-            <Form.Item
-                name="password"
-                rules={[
-                    {
-                        required: true,
-                        message: 'Please input your Password!',
-                    },
-                ]}
-            >
-                <Input prefix={<LockOutlined />} type="password" placeholder="Password" />
-            </Form.Item>
-            <Form.Item>
-                <Flex justify="space-between" align="center">
-                    <Form.Item name="remember" valuePropName="checked" noStyle>
-                        <Checkbox>Remember me</Checkbox>
-                    </Form.Item>
-                </Flex>
-            </Form.Item>
-
-            <Form.Item>
-                <Button block type="primary" htmlType="submit">
-                    Log in
-                </Button>
-                or <Link to='/register'>Register now!</Link>
-            </Form.Item>
+    
+          </Form.Item>
+    
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-form-button">
+              Log in
+            </Button>
+            Or <Link to='/register'>register now!</Link>
+          </Form.Item>
         </Form>
-    );
+      );
 };
 
 export default LoginPage;
-
-
-
