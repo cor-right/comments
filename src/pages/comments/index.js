79d7909f7975780e1cb5d856d0c0515d71ab47b6
zwy <zhongweiyu0812@163.com>
2025-03-18 23:19:53 +0800

    feat: 焦点获取

--- src/pages/comments/index.js
+++ src/pages/comments/index.js
@@ -1,63 +1,99 @@
 import "antd/dist/antd.min.css";
 import { Avatar, Button, Comment, Form, Input, List } from 'antd';
 import moment from 'moment';
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 const { TextArea } = Input;
+
+/**
+ * 评论列表
+ * @param {*} param0 
+ * @returns 
+ */
 const CommentList = ({ comments }) => (
     <List
         dataSource={comments}
-        header={`${comments.length} ${comments.length > 1 ? 'replies' : 'reply'}`}
+        header={`${comments.length} ${'条评论'}`}
         itemLayout="horizontal"
         renderItem={(props) => <Comment {...props} />}
     />
 );
-const Editor = ({ onChange, onSubmit, submitting, value }) => (
+
+// 评论提交
+const Editor = ({ onChange, onSubmit, submitting, value, sharedProps }) => (
     <>
         <Form.Item>
-            <TextArea rows={4} onChange={onChange} value={value} />
+            <TextArea rows={4} onChange={onChange} value={value} {...sharedProps} />
         </Form.Item>
         <Form.Item>
             <Button htmlType="submit" loading={submitting} onClick={onSubmit} type="primary">
-                Add Comment
+                评论发布
             </Button>
         </Form.Item>
     </>
 );
+
+
 const CommentsPage = () => {
+    // 评论列表更新
     const [comments, setComments] = useState([]);
+    // 提交状态更新
     const [submitting, setSubmitting] = useState(false);
-    const [value, setValue] = useState('');
+    const [currentComment, setCommentValue] = useState('');
+
+    // 焦点
+    const inputRef = useRef(null);
+
+    const sharedProps = {
+        ref: inputRef,
+        // rows:4,
+        //  onChange={onChange} value={value} 
+    };
+
+
+    // 提交触发
     const handleSubmit = () => {
-        if (!value) return;
+        if (!currentComment) return;
+
         setSubmitting(true);
         setTimeout(() => {
             setSubmitting(false);
-            setValue('');
+            setCommentValue('');
             setComments([
                 ...comments,
                 {
                     author: 'Han Solo',
                     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-                    content: <p>{value}</p>,
+                    content: <p>{currentComment}</p>,
                     datetime: moment('2016-11-22').fromNow(),
+                    actions: [<span key="comment-list-reply-to-0" onClick={onReplyClick}>Reply to</span>],
                 },
             ]);
         }, 1000);
     };
+
     const handleChange = (e) => {
-        setValue(e.target.value);
+        setCommentValue(e.target.value);
     };
+
+    // reply点击监听
+    const onReplyClick = () => {
+        inputRef.current.focus({
+            cursor: 'start',
+        });
+    }
+
     return (
         <>
             {comments.length > 0 && <CommentList comments={comments} />}
             <Comment
-                avatar={<Avatar src="https://joeschmoe.io/api/v1/random" alt="Han Solo" />}
+                avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" alt="Han Solo" />}
                 content={
                     <Editor
                         onChange={handleChange}
                         onSubmit={handleSubmit}
                         submitting={submitting}
-                        value={value}
+                        value={currentComment}
+                        sharedProps={sharedProps}
                     />
                 }
             />
